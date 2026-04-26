21b992fe3a3e6add928aa990ce127bb32ccf1607
kentakom1213 <kentakom1213@gmail.com>
2026-04-27 01:03:50 +0900

    add: アプリの更新

--- raspi-web-go/cmd/app/main.go
+++ raspi-web-go/cmd/app/main.go
@@ -7,12 +7,15 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hello from raspi zero")
-	})
+	http.HandleFunc("/", handler)
 
 	addr := "127.0.0.1:8080"
 	log.Println("listening on", addr)
 
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	email := r.Header.Get("Cf-Access-Authenticated-User-Email")
+	fmt.Fprintf(w, "hello %s\n", email)
 }
